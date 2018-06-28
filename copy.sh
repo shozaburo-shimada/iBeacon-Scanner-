ae1da0d9bc13f484a727c4da3c0165fb84ce29c2
Shozaburo Shimada <shozaburo@piccolo.co>
2018-06-28 14:30:07 +0900

    class化

---
+++ copy.sh
@@ -0,0 +1,2 @@
+scp ./blescan.py pi@192.168.250.42:~/Documents/iBeacon-Scanner-
+scp ./testblescan.py pi@192.168.250.42:~/Documents/iBeacon-Scanner-
--- testblescan.py
+++ testblescan.py
@@ -1,26 +1,25 @@
 # test BLE Scanning software
 # jcs 6/8/2014
 
-import blescan
+from blescan import BleScan
 import sys
-
 import bluetooth._bluetooth as bluez
+bs = BleScan()
 
 dev_id = 0
 try:
-	sock = bluez.hci_open_dev(dev_id)
-	print "ble thread started"
+    sock = bluez.hci_open_dev(dev_id)
+    print "ble thread started"
 
 except:
-	print "error accessing bluetooth device..."
-    	sys.exit(1)
+    print "error accessing bluetooth device..."
+    sys.exit(1)
 
-blescan.hci_le_set_scan_parameters(sock)
-blescan.hci_enable_le_scan(sock)
+bs.hci_le_set_scan_parameters(sock)
+bs.hci_enable_le_scan(sock)
 
 while True:
-	returnedList = blescan.parse_events(sock, 10)
-	print "----------"
-	for beacon in returnedList:
-		print beacon
-
+    returnedList = bs.parse_events(sock, 10)
+    print "----------"
+    for beacon in returnedList:
+        print beacon
